6791336761129c187bf3c1b904966fa44c1a88e4
egi <daegi01@gmail.com>
2020-03-23 21:40:43 +0100

    bumped version

--- RealisticWalkingSpeed/Mod.cs
+++ RealisticWalkingSpeed/Mod.cs
@@ -17,7 +17,7 @@ namespace RealisticWalkingSpeed
         public string SystemName = "RealisticWalkingSpeed";
         public string Name => "Realistic Walking Speed";
         public string Description => "Adjusts pedestrian walking speeds to realistic values.";
-        public string Version => "1.2.0";
+        public string Version => "1.2.1";
 
         public void OnEnabled()
         {
